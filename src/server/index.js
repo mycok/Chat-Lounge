fe3da714e88d2fb47a95219349346cfddc1d85f1
myko <awesomeme155@gmail.com>
2020-04-16 20:56:49 +0300

    chore: Add logging functionality
    - install and configure winston logger
    
    [Finishes #172361073]

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,9 @@
 import app from './Express';
 import config from '../config';
+import logger from '../utils/logger';
 
 const { port } = config;
 
 app.listen(port, () => {
-  console.log(`*****listening on port ${port}*****`);
+  logger.info(`*****listening on port ${port}*****`);
 });
